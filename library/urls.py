9e275c77a7f7b8141fecc395c18575339f469ce8
bamgm14 <bamgm14@gmail.com>
2021-09-24 11:33:18 +0530

    Preview Build v0.2

--- library/urls.py
+++ library/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('books/', views.library_details, name = 'library_details'),
     path('books/<slug:book_Slug>', views.book_details, name = "book-details"),
     path('', views.homepage, name = "home-page"),
-    path('contact/', views.contact, name = 'contact')
+    path('contact/', views.contact, name = 'contact'),
+    path('login/',views.Login, name = "login"),
+    path('register/',views.register, name = "register"),
+    #path('account/',views.account, name = "account")
 ]
--- library/views.py
+++ library/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import bookslist
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import NewUserForm
 def library_details(request):
     bookslists = bookslist.objects.all()
 
@@ -33,3 +37,34 @@ def book_details(request,book_Slug):
     
 def contact(request):
     return render(request, 'Contact.html')
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        print(f'Register',form.is_valid())
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration Successful")
+            print(f'Register')
+            return redirect("/account")
+        else:
+            print("Hello World")
+    else:
+        form = NewUserForm()
+    return render(request,'Register.html',{'form': form})
+
+def Login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request, user)
+                messages.debug(request, f"You are now logged in as {username}")
+                return redirect("/account")
+    else:
+        form = AuthenticationForm()
+    return render(request,'Login.html',{'form': form})
